fc03b7a555cba2d574253cbf6418e0f16c41dab3
Ben Marshall <me@benmarshall.me>
2013-10-14 22:38:58 -0500

    New stuff.

--- demo/demo.js
+++ demo/demo.js
@@ -9,26 +9,33 @@ $(function() {
     categorySelect = $.craigsAPI( "buildSelect", {
       ary: $categories,
       attr: {
-        "id": "category"
+        "id": "category",
+        "multiple": "multiple"
       }
     } ),
     locationSelect = $.craigsAPI( "buildSelect", {
       ary: $locations,
       attr: {
-        "id": "location"
+        "id": "location",
+        "multiple": "multiple"
       }
     } ),
-    interval;
+    interval,
+    snd = new Audio( "notification.mp3" );
 
   function setup( c ) {
     $.craigsAPI( "apiURL", "demo.php" );
 
     if ( typeof localStorage.getItem( "location" ) === "string" ) {
-      locationSelect.val( localStorage.getItem( "location" ) );
+      locationSelect.val( $.parseJSON( localStorage.getItem( "location" ) ) );
     }
 
     if ( typeof localStorage.getItem( "category" ) === "string" ) {
-      categorySelect.val( localStorage.getItem( "category" ) );
+      categorySelect.val( $.parseJSON( localStorage.getItem( "category" ) ) );
+    }
+
+    if ( typeof localStorage.getItem( "crawltype" ) === "string" ) {
+      $( "#crawl-type" ).val( localStorage.getItem( "crawltype" ) );
     }
 
     $( "#category-select" ).html( categorySelect );
@@ -41,29 +48,42 @@ $(function() {
     return str.replace( new RegExp( find, "g" ), replace );
   }
 
-  function addListing( listing ) {console.log(listing);
+  function addListing( listing ) {
     var listings = $( "#listings" ),
       html,
       thumbnail,
       title,
+      meta,
       description,
-      social;
+      social,
+      favorite;
 
+    favorites = $( "<a />" ).addClass("favorite icon-star");
     thumbnail = $( "<div />" ).addClass( "thumbnail" );
     title = $( "<h3 />" )
       .addClass( "title" )
       .html( $( "<a />" )
-      .attr( "href", "[LINK]" )
+      .attr({
+        "href": "---LINK---",
+        "target": "_blank"
+      })
       .html( "---TITLE---" )  );
     description = $( "<p />" ).addClass( "description" ).html( "---DESCRIPTION---" );
     html = $( "<article />" ).addClass( "listing" ).attr( "id", "---LISTINGID---" );
 
-    social = "<a href='#' onclick=\"javascript: popup( 'https://twitter.com/intent/tweet?text=---TITLE--- ---LINK--- %23classifieds %23craigslist' )\" class='icon-twitter-sign social'></a>";
+    meta = $( "<div />" ).addClass( "meta" );
+    meta.html( "---LOCATION---" );
+
+    social = "<div class='thumbnails'></div>";
+    social += "<a href='#' onclick=\"javascript: popup( 'https://twitter.com/intent/tweet?text=---TITLE--- ---LINK--- %23classifieds %23craigslist' )\" class='icon-twitter-sign social'></a>";
     social += "<a href='#' onclick=\"javascript: popup( 'https://www.facebook.com/sharer/sharer.php?u=---LINK---' )\" class='icon-facebook-sign social'></a>";
     social += "<a href='#' onclick=\"javascript: popup( 'https://plus.google.com/share?url=---LINK---' )\" class='icon-google-plus-sign social'></a>";
+    social += "<div class='clr'></div>";
 
+    html.append( favorites );
     html.append( thumbnail );
     html.append( title );
+    html.append( meta );
     html.append( description );
     html.append( social );
 
@@ -71,44 +91,214 @@ $(function() {
       html.attr( "id", "listing-" + listing.listingID );
 
       html.html( replaceAll( "---TITLE---", listing.title, html.html() ) );
+      html.html( replaceAll( "---LOCATION---", listing.location, html.html() ) );
       html.html( replaceAll( "---DESCRIPTION---", listing.description, html.html() ) );
       html.html( replaceAll( "---LISTINGID---", listing.listingID, html.html() ) );
-      html.html( replaceAll( "---LINK---", encodeURIComponent(listing.link), html.html() ) );
+      html.html( replaceAll( "---LINK---", listing.link, html.html() ) );
 
       if ( $.isArray( listing.imgList ) && listing.imgList.length > 0 && listing.imgList[0] ) {
         $( ".thumbnail", html ).css({
           "background-image": "url(" + listing.imgList[0] + ")"
         });
+
+        $.each( listing.imgList, function(k, v) {
+          $( ".thumbnails ", html).append( $( "<div />" ).addClass( "sm-thumbnail" ).css({
+            "background-image": "url(" + v + ")"
+          }));
+        });
       }
 
       listings.prepend( html );
+
+      snd.play();
     }
   }
 
-  function crawl() {
-    var category = $( "#category" ).val(),
+  function crawl( category, location ) {
+    if ( !category ) {
+      category = $( "#category" ).val();
+    }
+
+    if ( !location ) {
       location = $( "#location" ).val();
+    }
 
-    $.craigsAPI( "getLatest", {
-      category: category,
-      location: location,
-    }, function( listing ) {
-      addListing( $.parseJSON( listing ) );
+    if ( $.isArray( location ) && $.isArray( category ) && location.length && category.length ) {
+      $.each( location, function( i, l ) {
+        $.each( category, function( i2, c ) {
+          $.craigsAPI( "getLatest", {
+            category: c,
+            location: l,
+          }, function( listing ) {
+            addListing( $.parseJSON( listing ) );
+          });
+        });
+      });
+    }
+  }
+
+  function crawlCategory( cat ) {
+    var cnt = 0;
+
+    if ( !cat ) {
+      cat = $( "#category" ).val();
+    }  
+
+    if ( $.isArray( cat ) && cat.length ) {
+      $.each( cat, function( index, value ) {
+        $.each( $.parseJSON( $locations ), function( i, d ) {
+          if ( typeof d.children === "object" ) {
+            $.each( d.children, function( i2, d2 ) {
+              cnt++;
+              window.setTimeout( function() {
+                crawl( [value] , [i2] );
+              }, $( "#crawl-rate" ).val() * cnt);
+            });
+          } else {
+            cnt++;
+            window.setTimeout( function() {
+              crawl( [value] , [i] );
+            }, $( "#crawl-rate" ).val() * cnt);
+          }
+        });
+      });
+    }
+  }
+
+  function crawlLocation( loc ) {
+    var cnt = 0;
+
+    if ( !loc ) {
+      loc = $( "#location" ).val();
+    }  
+
+    if ( $.isArray( loc ) && loc.length ) {
+      $.each( loc, function( index, value ) {
+        $.each( $.parseJSON( $categories ), function( i, d ) {
+          if ( typeof d.children === "object" ) {
+            $.each( d.children, function( i2, d2 ) {
+              cnt++;
+              window.setTimeout( function() {
+                crawl( [i2], [value] );
+              }, $( "#crawl-rate" ).val() * cnt);
+            });
+          } else {
+            cnt++;
+            window.setTimeout( function() {
+              crawl( [i], [value] );
+            }, $( "#crawl-rate" ).val() * cnt);
+          }
+        });
+      });
+    }
+  }
+
+  function crawlAll() {
+    var cnt = 0;
+
+    $.each( $.parseJSON( $locations ), function( i, d ) {
+      if ( typeof d.children === "object" ) {
+        $.each( d.children, function( i2, d2 ) {
+          $.each( $.parseJSON( $categories ), function( ic, dc ) {
+            if ( typeof dc.children === "object" ) {
+              $.each( dc.children, function( ic2, dc2 ) {
+                cnt++;
+                window.setTimeout( function() {
+                  crawl( ic2 , i2 );
+                }, $( "#crawl-rate" ).val() * cnt);
+              });
+            } else {
+              cnt++;
+              window.setTimeout( function() {
+                crawl( ic , i2 );
+              }, $( "#crawl-rate" ).val() * cnt);
+            }
+          });
+        });
+
+      } else {
+        $.each( $.parseJSON( $categories ), function( ic, dc ) {
+          crawl( ic , i2 );
+        });
+      }
     });
+  }
 
-    localStorage.setItem( "location", location );
-    localStorage.setItem( "category", category );
+  function doCrawl() {
+    var type = $( "#crawl-type" );
+    switch ( type.val() ) {
+    case "normal":
+      crawl();
+      break;
+    case "everything":
+      crawlAll();
+      break;
+    case "category":
+      crawlCategory();
+      break;
+    case "location":
+      crawlLocation();
+      break;
+    }
+  }
+
+  function stopCrawl() {
+    var id = window.setTimeout( function() {}, 0 );
+
+    while ( id-- ) {
+      window.clearTimeout( id );
+    }
+    window.clearInterval( interval );
+  }
+
+  function startCrawl() {
+    var type = $( "#crawl-type" );
+
+    localStorage.setItem( "crawltype", type.val() );
+    localStorage.setItem( "location", JSON.stringify( $( "#location" ).val() ) );
+    localStorage.setItem( "category", JSON.stringify( $( "#category" ).val() ) );
+
+    interval = window.setInterval( function() {
+      doCrawl();
+    }, $( "#crawl-rate" ).val() * 4 );
   }
 
   setup( function() {
-    crawl();
-    interval = setInterval( function() {
-      crawl();
-    }, 3000 );
+    doCrawl();
+
+    startCrawl();
+  });
+
+  $( "#crawl-rate" ).bind( "change", function() {
+    stopCrawl();
+    doCrawl();
+    startCrawl();
+  });
+
+  $( "#crawl-type" ).bind( "change", function() { 
+    if ( $( this ).val() === "category" ) {
+      $( "#location" ).attr( "disabled", true );
+      $( "#category" ).attr( "disabled", false );
+    } else if ( $( this ).val() === "normal" ) {
+      $( "#location" ).attr( "disabled", false );
+      $( "#category" ).attr( "disabled", false );
+    } else if ( $( this ).val() === "everything" ) {
+      $( "#location" ).attr( "disabled", true );
+      $( "#category" ).attr( "disabled", true );
+    } else if ( $( this ).val() === "location" ) {
+      $( "#location" ).attr( "disabled", false );
+      $( "#category" ).attr( "disabled", true );
+    }
+
+    stopCrawl();
+    doCrawl();
+    startCrawl();
   });
 
   $( "#category, #location" ).bind( "change", function() {
-    crawl();
+    stopCrawl();
+    doCrawl();
+    startCrawl();
   });
 
   $( ".toggle-settings" ).bind( "click", function( e ) {
@@ -116,7 +306,7 @@ $(function() {
     var nav = $( ".nav" );
     if ( nav.css( "bottom" ) === "0px" ) {
       $( ".nav" ).css({
-        bottom: "-6rem"
+        bottom: "-27rem"
       });
     } else {
       $( ".nav" ).css({
--- jquery.craigsAPI.js
+++ jquery.craigsAPI.js
@@ -50,23 +50,452 @@
         }
       }
     },
+    "ppp": {
+      "name": "Personals",
+      "allow_all": true,
+      "children": {
+        "stp": {
+          "name": "Strictly Platonic"
+        },
+        "w4w": {
+          "name": "Women Seeking Women"
+        },
+        "w4m": {
+          "name": "Women Seeking Men"
+        },
+        "m4w": {
+          "name": "Men Seeking Women"
+        },
+        "m4m": {
+          "name": "Men Seeking Men"
+        },
+        "msr": {
+          "name": "Miscellaneous Romance"
+        },
+        "cas": {
+          "name": "Casual Encounters"
+        },
+        "mis": {
+          "name": "Missed Connections"
+        },
+        "rnr": {
+          "name": "Rants &amp; Raves"
+        }
+      }
+    },
     "hhh": {
       "name": "Housing",
       "allow_all": true,
       "children": {
         "apa": {
           "name": "Apartment &amp; Housing for Rent"
+        },
+        "roo": {
+          "name": "Rooms &amp; Shares"
+        },
+        "sub": {
+          "name": "Sublets &amp; Temporary Housing"
+        },
+        "hsw": {
+          "name": "Housing Wanted"
+        },
+        "swp": {
+          "name": "Housing Swap"
+        },
+        "vac": {
+          "name": "Vacation Rentals"
+        },
+        "prk": {
+          "name": "Parking &amp; Storage"
+        },
+        "off": {
+          "name": "Office &amp; Commercial"
+        },
+        "rea": {
+          "name": "Real Estate"
+        },
+        "sga": {
+          "name": "Sporting Goods"
+        },
+        "tia": {
+          "name": "Tickets"
+        },
+        "tla": {
+          "name": "Tools"
+        },
+        "waa": {
+          "name": "Wanted"
+        },
+        "ppa": {
+          "name": "Appliances"
+        },
+        "ara": {
+          "name": "Arts &amp; Crafts"
+        },
+        "sna": {
+          "name": "ATVs, UTVs, Snowmobiles"
+        },
+        "pta": {
+          "name": "Auto Parts"
+        }
+      }
+    },
+    "sss": {
+      "name": "For Sale / Wanted",
+      "allow_all": true,
+      "children": {
+        "ata": {
+          "name": "Antiques"
+        },
+        "baa": {
+          "name": "Baby &amp; Kid Stuff"
+        },
+        "bar": {
+          "name": "Barter"
+        },
+        "bia": {
+          "name": "Bicycles"
+        },
+        "boats": {
+          "name": "Boats"
+        },
+        "bka": {
+          "name": "Books &amp; Magazines"
+        },
+        "bfa": {
+          "name": "Business"
+        },
+        "sya": {
+          "name": "Computers"
+        },
+        "zip": {
+          "name": "Free Stuff"
+        },
+        "fua": {
+          "name": "Furniture"
+        },
+        "foa": {
+          "name": "General for Sale"
+        },
+        "hsa": {
+          "name": "Household Items"
+        },
+        "jwa": {
+          "name": "Jewelry"
+        },
+        "maa": {
+          "name": "Materials"
+        },
+        "rva": {
+          "name": "Recreational Vehicles"
+        },
+        "haa": {
+          "name": "Health &amp; Beauty"
+        },
+        "cto": {
+          "name": "Cars &amp; Trucks (By Owner)"
+        },
+        "ctd": {
+          "name": "Cars &amp; Trucks (By Dealer)"
+        },
+        "cta": {
+          "name": "Cars &amp; Trucks"
+        },
+        "ema": {
+          "name": "CDs / DVDs / VHS"
+        },
+        "moa": {
+          "name": "Cell Phones"
+        },
+        "cla": {
+          "name": "Clothing &amp; Accessories"
+        },
+        "cba": {
+          "name": "Collectibles"
+        },
+        "ela": {
+          "name": "Electronics"
+        },
+        "gra": {
+          "name": "Farm &amp; Garden"
+        },
+        "gms": {
+          "name": "Garage &amp; Moving Sales"
+        },
+        "hva": {
+          "name": "Heavy Equipment"
+        },
+        "mca": {
+          "name": "Motorcycles &amp; Scooters"
+        },
+        "mcy": {
+          "name": "Motorcycles &amp; Scooters (By Owner)"
+        },
+        "mcd": {
+          "name": "Motorcycles &amp; Scooters (By Dealer)"
+        },
+        "mpa": {
+          "name": "Motorcycle Parts &amp; Accessories"
+        },
+        "mpo": {
+          "name": "Motorcycle Parts &amp; Accessories (By Owner)"
+        },
+        "mpd": {
+          "name": "Motorcycle Parts &amp; Accessories (By Dealer)"
+        },
+        "msa": {
+          "name": "Musical Instruments"
+        },
+        "pha": {
+          "name": "Photo &amp; Video"
+        },
+        "taa": {
+          "name": "Toys &amp; Games"
+        },
+        "vga": {
+          "name": "Video Gaming"
         }
       }
     }
   },
   $locations = {
+    "al": {
+      "name": "Alabama",
+      "allow_all": false,
+      "children": {
+        "auburn": {
+          "name": "Auburn"
+        },
+        "bham": {
+          "name": "Birmingham"
+        },
+        "dothan": {
+          "name": "Dothan"
+        },
+        "shoals": {
+          "name": "Florence / Muscle Shoals"
+        },
+        "gadsden": {
+          "name": "Gadsden-Anniston"
+        },
+        "huntsville": {
+          "name": "Huntsville / Decatur"
+        },
+        "mobile": {
+          "name": "Mobile"
+        },
+        "montgomery": {
+          "name": "Montgomery"
+        },
+        "tuscaloosa": {
+          "name": "Tuscaloosa"
+        }
+      }
+    },
+    "ak": {
+      "name": "Alaska",
+      "allow_all": false,
+      "children": {
+        "anchorage": {
+          "name": "Anchorage / Mat-Su"
+        },
+        "fairbanks": {
+          "name": "Fairbanks"
+        },
+        "kenai": {
+          "name": "Kenai Peninsula"
+        },
+        "juneau": {
+          "name": "Southeast Alaska"
+        }
+      }
+    },
+    "az": {
+      "name": "Arizona",
+      "allow_all": false,
+      "children": {
+        "flagstaff": {
+          "name": "Flagstaff / Sedona"
+        },
+        "mohave": {
+          "name": "Mohave County"
+        },
+        "phoenix": {
+          "name": "Phoenix"
+        },
+        "prescott": {
+          "name": "Prescott"
+        },
+        "showlow": {
+          "name": "Show Low"
+        },
+        "sierravista": {
+          "name": "Sierra Vista"
+        },
+        "tucson": {
+          "name": "Tucson"
+        },
+        "yuma": {
+          "name": "Yuma"
+        }
+      }
+    },
+    "ar": {
+      "name": "Arkansas",
+      "allow_all": false,
+      "children": {
+        "fayar": {
+          "name": "Fayetteville"
+        },
+        "fortsmith": {
+          "name": "Fort Smith"
+        },
+        "jonesboro": {
+          "name": "Jonesboro"
+        },
+        "littlerock": {
+          "name": "Little Rock"
+        },
+        "texarkana": {
+          "name": "Texarkana"
+        }
+      }
+    },
+    "ca": {
+      "name": "California",
+      "allow_all": false,
+      "children": {
+        "bakersfield": {
+          "name": "Bakersfield"
+        },
+        "chico": {
+          "name": "Chico"
+        },
+        "fresno": {
+          "name": "Fresno"
+        },
+        "goldcountry": {
+          "name": "Gold Country"
+        },
+        "hanford": {
+          "name": "Hanford-Corcoran"
+        },
+        "humboldt": {
+          "name": "Humboldt County"
+        },
+        "imperial": {
+          "name": "Imperial County"
+        },
+        "inlandempire": {
+          "name": "Inland Empire"
+        },
+        "losangeles": {
+          "name": "Los Angeles"
+        },
+        "mendocino": {
+          "name": "Mendocino County"
+        },
+        "merced": {
+          "name": "Merced"
+        },
+        "modesto": {
+          "name": "Modesto"
+        },
+        "monterey": {
+          "name": "Monterey Bay"
+        },
+        "orangecounty": {
+          "name": "Orange County"
+        }
+      }
+    },
     "tx": {
       "name": "Texas",
       "allow_all": false,
       "children": {
+        "abilene": {
+          "name": "Abilene"
+        },
+        "amarillo": {
+          "name": "Amarillo"
+        },
         "austin": {
           "name": "Austin"
+        },
+        "beaumont": {
+          "name": "Beaumont / Port Arthur"
+        },
+        "brownsville": {
+          "name": "Brownsville"
+        },
+        "collegestation": {
+          "name": "College Station"
+        },
+        "corpuschristi": {
+          "name": "Corpus Christi"
+        },
+        "dallas": {
+          "name": "Dallas / Fort Worth"
+        },
+        "dallas": {
+          "name": "Dallas / Fort Worth"
+        },
+        "nacogdoches": {
+          "name": "Deep East Texas"
+        },
+        "delrio": {
+          "name": "Del Rio / Eagle Pass"
+        },
+        "elpaso": {
+          "name": "El Paso"
+        },
+        "galveston": {
+          "name": "Galveston"
+        },
+        "houston": {
+          "name": "Houston"
+        },
+        "killeen": {
+          "name": "Killen / Temple / Ft. Hood"
+        },
+        "laredo": {
+          "name": "Laredo"
+        },
+        "lubbock": {
+          "name": "Lubbock"
+        },
+        "mcallen": {
+          "name": "McAllen / Edinburg"
+        },
+        "odessa": {
+          "name": "Odessa / Midland"
+        },
+        "sanangelo": {
+          "name": "San Angelo"
+        },
+        "sanantonio": {
+          "name": "San Antonio"
+        },
+        "sanmarcos": {
+          "name": "San Marcos"
+        },
+        "bigbend": {
+          "name": "Southwest Texas"
+        },
+        "texoma": {
+          "name": "Texoma"
+        },
+        "easttexas": {
+          "name": "Tyler / East Texas"
+        },
+        "victoriatx": {
+          "name": "Victoria"
+        },
+        "waco": {
+          "name": "Waco"
+        },
+        "wichitafalls": {
+          "name": "Wichita Falls"
         }
       }
     }
@@ -119,11 +548,12 @@
   };
 
   // Get the latest listing
-  methods.getLatest = function ( location, category, callback ) {
+  methods.getLatest = function( location, category, callback ) {
     $.post( $apiURL, { action: "getLatest", location: location, category: category }, function( d ) {
       callback( d );
     });
   };
+
 
   // API calls
   $.craigsAPI = function ( method, options, callback ) {
